c2f93670ab6e71ac5b7bbacfd0df9adfe6823da3
umirzakov01 <yerzhan.umirzakov@nu.edu.kz>
2023-08-28 00:15:55 +0600

    second

--- homework1.js
+++ homework1.js
@@ -1,1 +1,21 @@
-console.log(10)
+const prompt = require ("prompt-sync")({sigint: true})
+const operator = prompt('Enter operator: ');  
+  
+const number1 = parseFloat(prompt ('Enter the first number: '));  
+const number2 = parseFloat(prompt ('Enter the second number: '));  
+  
+let result;  
+  
+if (operator == '+') { 
+    result = number1 + number2;  
+}  
+else if (operator == '-') { 
+    result = number1 - number2;  
+}  
+else if (operator == '*') {   
+    result = number1 * number2;  
+}  
+else {  
+    result = number1 / number2;  
+}  
+console.log("The result is ", result)
